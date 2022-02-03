a19d369f9edf87eb4ec7520c741f17b4d3db7601
NAKWONCHOIDESK1\NakWonCHOI <ch0494@gmail.com>
2022-02-03 20:03:14 +0900

    5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,8 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   staticDirs: ["../public"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-interactions",
-  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/preset-create-react-app", "@storybook/addon-interactions"],
+  core: {
+    builder: "webpack5"
+  }
 };
